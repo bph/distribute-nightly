aa487911f1e4db638d2e8e1abbc533b406a69dab
bph <birgit.pauli@gmail.com>
2021-01-11 22:09:00 -0500

    not yet successful ftp

--- utils/ftp.js
+++ utils/ftp.js
@@ -1,5 +1,7 @@
 /**
  * sftp -o Port=2222 live.f5534b71-d1e9-4b6f-a86f-fa78778ad9cf@appserver.live.f5534b71-d1e9-4b6f-a86f-fa78778ad9cf.drush.in
+ * local directory: /Users/birgit/gb-nightly/gutenberg/
+ * remote directory: /files/2020/11/
  * 
  */
 
@@ -10,7 +12,7 @@ module.exports = (async () => {
     example();
 
     async function example() {
-        const client = new ftp.Client();
+        const client = new ftp.Client(timeout = 30000);
         client.ftp.verbose = true;
         
         try{
@@ -18,7 +20,8 @@ module.exports = (async () => {
                 host: process.env.FTPhost,
                 user: process.env.FTPuser,
                 password: process.env.FTPpass,
-                secure: true   
+                port: process.env.FTPport,
+                secure: true
             });
             console.log(await client.list());
             await client.uploadFrom(`../gutenberg/gutenberg.zip`, `/files/2020/11/gutenberg.zip`);
